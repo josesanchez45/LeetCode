ed2fe5dc7a51cf0652f555831131a3e14130fc2c
jose sanchez <jose.sanchez.artist@gmail.com>
2022-12-12 23:45:45 -0800

    additional problems added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿public class Solution
+﻿using System.Data.SqlTypes;
+using System.Numerics;
+
+public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
@@ -31,12 +34,53 @@
         int[] countWays = new int[n + 1];
         countWays[1] = 1;
         countWays[2] = 2;
-        for (int i = 3; i <=  n ; i++)
+        for (int i = 3; i <= n; i++)
         {
             countWays[i] = countWays[i - 1] + countWays[i - 2];
         }
         return countWays[n];
     }
+    public int MinFallingPathSum(int[][] matrix)
+    {
+        for(int i = 1; i < matrix.Length; i++)
+        {
+            for(int j = 0; j < matrix[0].Length; j++)
+            {
+                int min = matrix[i - 1][j];
+                if (j - 1 >= 0) min = Math.Min(min, matrix[i - 1][j - 1]);
+                if(j+1 < matrix[0].Length) min = Math.Min(min,matrix[i-1][j+1]);
+                matrix[i][j] += min;
+            }
+        }
+        return matrix[matrix.Length - 1].Min();
+    }
+    public bool IsPalindrome(int x)
+    {
+        if( x == 0)
+        {
+            return false;   
+        }
+        int remainder, sum = 0;
+        int temp = x;
 
+        while(x > 0)
+        {
+            remainder = x % 10;
+
+            sum =(sum*10) + remainder;
+
+            x = x /10;
+        }
+        if (temp == sum)
+        {
+            return true;
+        }
+        else
+            return false;
+
+
+
+    }
 }
+
 
